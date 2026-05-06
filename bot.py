ff10550d283d2bbf40f4be01c5bb01d7ed23f67f
dileepkumar724856-byte <dileepkumar724856@gmail.com>
2026-05-06 11:25:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,42 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
-from telegram import Update, BotCommand
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os .getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
 
-async def set_menu(app):
-    commands = [
-        BotCommand("start", "🚀 Start"),
-        BotCommand("notes", "📚 Notes"),
-        BotCommand("quiz", "🧠 Quiz"),
-    ]
-    await app.bot.set_my_commands(commands)
+# MENU BUTTONS
+MENU = [
+    ["📚 Notes", "🧠 Test"],
+    ["💎 Premium", "❓ Help"]
+]
 
+# START
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome 🚀")
+    await update.message.reply_text(
+        "🚀 Welcome to PulsePrep NEET",
+        reply_markup=ReplyKeyboardMarkup(MENU, resize_keyboard=True)
+    )
 
+# BUTTON HANDLER
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "📚 Notes":
+        await update.message.reply_text("📚 Biology Notes Coming Soon")
+
+    elif text == "🧠 Test":
+        await update.message.reply_text("🧠 Daily Test Coming Soon")
+
+    elif text == "💎 Premium":
+        await update.message.reply_text("💎 Premium access soon")
+
+    elif text == "❓ Help":
+        await update.message.reply_text("Use buttons below 👇")
+
+# APP
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start",start))
-app.post_init = set_menu
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 app.run_polling()
