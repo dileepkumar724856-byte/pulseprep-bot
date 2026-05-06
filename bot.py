b983842077d3b008f7a44c0419a12bd064154c6b
dileepkumar724856-byte <dileepkumar724856@gmail.com>
2026-05-06 12:50:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+
+
 from telegram import (
     Update,
     ReplyKeyboardMarkup,
@@ -45,22 +47,19 @@ MENU = [
 
 # START
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     await update.message.reply_text(
-        "🚀 Welcome to PulsePrep NEET",
+        "🚀 Welcome to PulsePrep NEET Bot",
         reply_markup=ReplyKeyboardMarkup(MENU, resize_keyboard=True)
     )
 
 # SEND QUIZ
-async def send_quiz(update, context, q_index=0):
-
-    question = quiz_data[q_index]
+async def send_quiz(update, context):
+    question = quiz_data[0]
 
     keyboard = []
-
     for option in question["options"]:
         keyboard.append(
-            [InlineKeyboardButton(option, callback_data=f"{q_index}|{option}")]
+            [InlineKeyboardButton(option, callback_data=option)]
         )
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -70,47 +69,38 @@ async def send_quiz(update, context, q_index=0):
         reply_markup=reply_markup
     )
 
-# BUTTON MENU HANDLER
+# HANDLE MENU
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     text = update.message.text
 
     if text == "📚 Notes":
-        await update.message.reply_text("📚 Biology Notes Coming Soon")
+        await update.message.reply_text("📚 Notes coming soon")
 
     elif text == "🧠 Test":
         await send_quiz(update, context)
 
     elif text == "💎 Premium":
-        await update.message.reply_text("💎 Premium access soon")
+        await update.message.reply_text("💎 Premium soon")
 
     elif text == "❓ Help":
         await update.message.reply_text("Use buttons below 👇")
 
-# QUIZ BUTTON CHECK
+# QUIZ BUTTON
 async def quiz_button(update, context):
 
     query = update.callback_query
     await query.answer()
 
-    data = query.data.split("|")
-
-    q_index = int(data[0])
-    selected = data[1]
-
-    correct = quiz_data[q_index]["answer"]
+    selected = query.data
+    correct = quiz_data[0]["answer"]
 
     if selected == correct:
         await query.message.reply_text("✅ Correct Answer")
-
     else:
-        await query.message.reply_text(
-            f"❌ Wrong Answer\nCorrect Answer: {correct}"
-        )
+        await query.message.reply_text(f"❌ Wrong\nCorrect: {correct}")
 
-   await query.message.reply_text("🏁 Quiz Finished")
-
-# APP
+# APP START
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
@@ -123,4 +113,4 @@ app.add_handler(CallbackQueryHandler(quiz_button))
 
 print("Bot Running...")
 
-app.run_polling(stop_signals=None)
+app.run_polling(close_loop=False😎
