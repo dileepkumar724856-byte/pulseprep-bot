d0b465fc048e54525175f0a174abcc7e5c96ff43
dileepkumar724856-byte <dileepkumar724856@gmail.com>
2026-05-08 00:39:24 +0530

    Update quiz.py

--- features/quiz.py
+++ features/quiz.py
@@ -1,28 +1,29 @@
 import random
-from questions.leader_test import leader_test
 
 from questions.units_questions import units_questions
+from questions.leader_test import leader_test
+
 
 async def send_quiz(update):
-    async def send_leader_test(update):
-
-    import random
-
-    q = random.choice(leader_test)
-
-    await update.message.reply_poll(
-        question=f"🔥 {q['question']}",
-        options=q["options"],
-        type="quiz",
-        correct_option_id=q["answer"],
-        explanation=q["explanation"],
-        is_anonymous=False
-    )
 
     q = random.choice(units_questions)
 
     await update.message.reply_poll(
         question=f"⚡ {q['question']}\n\n📘 {q['year']}",
+        options=q["options"],
+        type="quiz",
+        correct_option_id=q["answer"],
+        explanation=f"✅ {q['explanation']}",
+        is_anonymous=False
+    )
+
+
+async def send_leader_test(update):
+
+    q = random.choice(leader_test)
+
+    await update.message.reply_poll(
+        question=f"🔥 {q['question']}",
         options=q["options"],
         type="quiz",
         correct_option_id=q["answer"],
