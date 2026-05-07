591c4167bfa7947e53e37c2fd56a84c2363e9105
dileepkumar724856-byte <dileepkumar724856@gmail.com>
2026-05-08 00:32:53 +0530

    Create leader_test.py

---
+++ questions/leader_test.py
@@ -0,0 +1,42 @@
+leader_test = [
+
+{
+    "question": "A screw gauge has a least count of 0.01 mm. If pitch decreases by 25% and circular divisions increase by 50%, new least count is?",
+    "options": [
+        "1 × 10⁻³ mm",
+        "2.5 × 10⁻³ mm",
+        "2 × 10⁻³ mm",
+        "5 × 10⁻³ mm"
+    ],
+    "answer": 3,
+    "explanation": "Correct answer is 5 × 10⁻³ mm",
+    "year": "LEADER TEST"
+},
+
+{
+    "question": "Percentage error in equivalent resistance in parallel combination?",
+    "options": [
+        "2.33%",
+        "4.33%",
+        "5.33%",
+        "6.33%"
+    ],
+    "answer": 0,
+    "explanation": "Correct error propagation gives 2.33%",
+    "year": "LEADER TEST"
+},
+
+{
+    "question": "Order of magnitude of 2.3 × 10⁵ is?",
+    "options": [
+        "3",
+        "5",
+        "7",
+        "9"
+    ],
+    "answer": 1,
+    "explanation": "Nearest power is 10⁵",
+    "year": "LEADER TEST"
+}
+
+]
